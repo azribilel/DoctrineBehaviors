8bf51df51e0198f94ba06ad3e2adc997c0c14f22
Azri Bilel <bilel.azri@efipeek.com>
2023-10-04 16:55:49 +0200

    Fix(php-types): fix return type in getContainerExtension

--- src/Bundle/DoctrineBehaviorsBundle.php
+++ src/Bundle/DoctrineBehaviorsBundle.php
@@ -4,6 +4,7 @@ namespace Knp\DoctrineBehaviors\Bundle;
 
 use Knp\DoctrineBehaviors\Bundle\DependencyInjection\DoctrineBehaviorsExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 class DoctrineBehaviorsBundle extends Bundle
 {
